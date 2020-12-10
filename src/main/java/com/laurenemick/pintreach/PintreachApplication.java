150bcc44113e75936f243bd5f2fbfa3f2724e122
Lauren Emick <laurenemick6@gmail.com>
2020-12-10 11:39:33 -0800

    configures  application

---
+++ src/main/java/com/laurenemick/pintreach/PintreachApplication.java
@@ -0,0 +1,16 @@
+package com.laurenemick.pintreach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PintreachApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PintreachApplication.class,
+            args);
+    }
+
+}
---
+++ src/test/java/com/laurenemick/pintreach/PintreachApplicationTests.java
@@ -0,0 +1,15 @@
+package com.laurenemick.pintreach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PintreachApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
